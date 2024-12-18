04dacddc5107c470a107499cac2ac8f58c419420
Brian Uceda <monalek22@gmail.com>
2024-12-18 01:06:59 -0500

    feat(dev): add nginx.conf and docker-compose.yml

--- src/main/java/com/brianuceda/springboot_testing/controllers/TestController.java
+++ src/main/java/com/brianuceda/springboot_testing/controllers/TestController.java
@@ -1,5 +1,6 @@
 package com.brianuceda.springboot_testing.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class TestController {
+    @Value("${app.production}")
+    private Boolean production;
     
     @GetMapping("/test")
     public String test() {
-        return "Hello World";
+        return "Welcome from Spring Boot Application. Production: " + production;
     }
 }
